e3374e312ec5dc7ef11da0e8a0040d99ec66e856
amiyakiran <amiyakiran@gmail.com>
2024-01-06 11:02:35 +0530

    connection string changed v2

--- DB/connection.js
+++ DB/connection.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose')
 const connectionString = process.env.DATABASE
 
 //connect node.js/server with mongodb -
-mongoose.connect(connectionString).then(()=>{
-    console.log(connectionString);
+mongoose.connect('mongodb+srv://amiyakiran196:mern@cluster0.f2ldtkl.mongodb.net/projectFairb4?retryWrites=true&w=majority').then(()=>{
+    /* console.log(typeof(connectionString)); */
     console.log('mongobd connected successfully');
 }).catch((err)=>{
     console.log(`mongodb failed to connect due to :${err}`);
